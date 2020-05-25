2d7576ec48a7fa97df6aa184cc6d18e6983c4856
gilbertm <gilbertm@mit.edu>
2020-05-25 11:29:21 -0400

    mock badges page

---
+++ web_scripts/static/js/badges.js
@@ -0,0 +1,8 @@
+function searchFunc() {
+    var search = document.getElementById("badge-search");
+    if (search.style.display == "flex") {
+        search.style.display = "none";
+    } else {
+        search.style.display = "flex";
+    }
+}
