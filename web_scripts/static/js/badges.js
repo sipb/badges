1326bd3dcbe3d61b1fcee038bdb72b28095408dc
gilbertm <gilbertm@mit.edu>
2020-05-25 17:51:59 -0400

    refactor search bar into common

--- web_scripts/static/js/badges.js
+++
@@ -1,8 +0,0 @@
-function searchFunc() {
-    var search = document.getElementById("badge-search");
-    if (search.style.display == "flex") {
-        search.style.display = "none";
-    } else {
-        search.style.display = "flex";
-    }
-}
