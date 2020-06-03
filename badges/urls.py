ff404d34d5c0388b73688e51d1117b251a487f7e
Michael Gilbert <gilbertm@mit.edu>
2020-06-03 17:32:14 -0400

    add urls

--- badges/urls.py
+++ badges/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'badges.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/', 'badgesapp.views.home', name='home'),
+    url(r'^badges/', 'badgesapp.views.badges', name='badges')
 )
