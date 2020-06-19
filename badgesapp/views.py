7cb13c49bf8a603e19edf5d4e41e0743f1e6188a
gbhc <59673621+gbhc@users.noreply.github.com>
2020-06-19 17:33:45 -0400

    new django app, now using the 3.0.7 instead of 1.0

--- badgesapp/views.py
+++ badgesapp/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
-def home(request):
-    return render(request, 'home.html')
 
-def badges(request):
-    return render(request, 'badges.html')
+def index(request):
+    return HttpResponse("Hello, world. You're at the badges index.")
