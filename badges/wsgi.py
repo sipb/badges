71be96940df6525aeaecd3f6d8b62f778d455320
Michael Gilbert <gilbertm@mit.edu>
2020-06-19 16:50:38 -0400

    Upgrade to work with Django 3.0.7

--- badges/wsgi.py
+++ badges/wsgi.py
@@ -4,11 +4,13 @@ WSGI config for badges project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "badges.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'badges.settings')
+
 application = get_wsgi_application()
